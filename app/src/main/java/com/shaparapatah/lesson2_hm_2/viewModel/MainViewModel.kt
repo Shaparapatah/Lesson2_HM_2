15e6e4450d4e90658eb772a6eaea1b715184c548
Shaparapatah <mm.fedorchenko@gmail.com>
2021-09-06 22:17:10 +0300

    Закончил 2-й урок. P.S. задание со *

--- app/src/main/java/com/shaparapatah/lesson2_hm_2/viewModel/MainViewModel.kt
+++ app/src/main/java/com/shaparapatah/lesson2_hm_2/viewModel/MainViewModel.kt
@@ -3,7 +3,9 @@ package com.shaparapatah.lesson2_hm_2.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.geekbrains.lesson_1423_2_2_main.repository.RepositoryImpl
+import java.lang.IllegalStateException
 import java.lang.Thread.sleep
+import kotlin.random.Random
 
 class MainViewModel(
     private val liveDataToObserver: MutableLiveData<AppState> = MutableLiveData(),
@@ -19,7 +21,11 @@ class MainViewModel(
         liveDataToObserver.postValue(AppState.Loading)
         Thread {
             sleep(2000)
+            val random = Random(15).nextInt()
+            if (random > 5)
             liveDataToObserver.postValue(AppState.Success(repositoryImpl.getWeatherFromRemoteSource()))
+            else
+                liveDataToObserver.postValue(AppState.Error(IllegalStateException()))
         }.start()
     }
 }
