8d9b5e3a39e6bff14a476c04024cc165039ff9b7
Shaparapatah <mm.fedorchenko@gmail.com>
2021-09-12 19:34:36 +0300

    Добавил обработчик ошибок в WeatherLoader.kt Перенёс ключ разработчки в статическую коснтанту

--- app/src/main/java/com/shaparapatah/lesson2_hm_2/repository/WeatherLoader.kt
+++ app/src/main/java/com/shaparapatah/lesson2_hm_2/repository/WeatherLoader.kt
@@ -2,10 +2,12 @@ package com.shaparapatah.lesson2_hm_2.repository
 
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 
 import com.google.gson.Gson
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.net.MalformedURLException
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
@@ -13,22 +15,43 @@ class WeatherLoader(
     private val listener: WeatherLoaderListener,
     private val lat: Double,
     private val lon: Double
+
 ) {
+
+    companion object {
+        private const val YOUR_API_KEY = "X-Yandex-API-Key"
+        private const val KEY = "06e57aba-81f3-4ca5-93d2-d50ce5b048a3"
+    }
+
     fun loadWeather() {
-        val url = URL("https://api.weather.yandex.ru/v2/informers?lat=${lat}&lon=${lon}")
-        Thread {
-            val urlConnection = url.openConnection() as HttpsURLConnection
-            urlConnection.requestMethod = "GET"
-            urlConnection.addRequestProperty(
-                "X-Yandex-API-Key",
-                "06e57aba-81f3-4ca5-93d2-d50ce5b048a3"
-            )
-            urlConnection.readTimeout = 10000
-            val reader = BufferedReader(InputStreamReader(urlConnection.inputStream))
-            val weatherDTO = Gson().fromJson(reader, WeatherDTO::class.java)
+        try {
+            val url = URL("https://api.weather.yandex.ru/v2/informers?lat=${lat}&lon=${lon}")
             val handler = Handler(Looper.getMainLooper())
-            handler.post { listener.onLoaded(weatherDTO) }
-            urlConnection.disconnect()
-        }.start()
+            val urlConnection = url.openConnection() as HttpsURLConnection
+            Thread {
+                try {
+
+                    urlConnection.requestMethod = "GET"
+                    urlConnection.addRequestProperty(
+                        YOUR_API_KEY,
+                        KEY
+                    )
+                    urlConnection.readTimeout = 10000
+                    val reader = BufferedReader(InputStreamReader(urlConnection.inputStream))
+                    val weatherDTO = Gson().fromJson(reader, WeatherDTO::class.java)
+
+                    handler.post { listener.onLoaded(weatherDTO) }
+                    urlConnection.disconnect()
+                } catch (e: Exception) {
+                    Log.e("", "Fail connection", e)
+                    e.printStackTrace()
+                } finally {
+                    urlConnection.disconnect()
+                }
+            }.start()
+        } catch (e: MalformedURLException) {
+            Log.e("", "Fail URI", e)
+            e.printStackTrace()
+        }
     }
 }
