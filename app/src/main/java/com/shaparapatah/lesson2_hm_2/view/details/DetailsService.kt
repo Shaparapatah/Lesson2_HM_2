c5eb7c1fdcc5896202079670dbd7021c25d56db9
Shaparapatah <mm.fedorchenko@gmail.com>
2021-09-16 23:29:15 +0300

    6 домашнее задание - начал

---
+++ app/src/main/java/com/shaparapatah/lesson2_hm_2/view/details/DetailsService.kt
@@ -0,0 +1,13 @@
+package com.shaparapatah.lesson2_hm_2.view.details
+
+import android.app.IntentService
+import android.content.Intent
+
+
+const val DETAILS_INTENT_FILTER = "DETAILS INTENT FILTER"
+
+class DetailsService(name: String = "details") : IntentService(name) {
+    override fun onHandleIntent(p0: Intent?) {
+            
+    }
+}
