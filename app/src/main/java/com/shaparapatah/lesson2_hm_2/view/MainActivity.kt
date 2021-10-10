368e8ba253149ed3d477d310adfecbdc92645c43
Shaparapatah <mm.fedorchenko@gmail.com>
2021-10-10 21:12:08 +0300

    11 дз начал и разделил на два потока, как Вы показывали на уроке. Вынес в отдельную ф-кцию

--- app/src/main/java/com/shaparapatah/lesson2_hm_2/view/MainActivity.kt
+++ app/src/main/java/com/shaparapatah/lesson2_hm_2/view/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun notificationsChannels() {
+    private fun notificationsChannels() { //ff
 
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
